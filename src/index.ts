596c122f09bde3292eb888cf884dd61664280fa6
David Montesdeoca <backpackerhh@gmail.com>
2024-01-24 11:58:09 +0100

    Allow to execute actions using eventing logic without awaiting
    Internally fetch and save functions are async, but at the moment of
    using that code, there is no need to await.

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,25 @@
 import { User } from "./models/User";
 
-async function init() {
-  const user = User.build({ id: "4ff8" });
+const user = User.build({ id: "4ff8" });
 
-  user.on("change", () => {
-    console.log(`User with ID ${user.get("id")} changed!`);
-  });
+user.on("change", () => {
+  console.log(`User with ID ${user.get("id")} fetched! -> ${user.get("name")}`);
+});
 
-  await user.fetch();
+user.fetch();
 
-  const existingUser = User.build({ id: "4f25", name: "David", age: 37 });
+const existingUser = User.build({ id: "4f25", name: "David", age: 37 });
 
-  existingUser.on("save", () => {
-    console.log(`User with ID ${existingUser.get("id")} saved!`);
-  });
+existingUser.on("save", () => {
+  console.log(`User with ID ${existingUser.get("id")} saved! -> ${existingUser.get("name")}`);
+});
 
-  await existingUser.save();
+existingUser.save();
 
-  // const newUser = User.build({ name: "Perico", age: 21 });
+// const newUser = User.build({ name: "Perico", age: 21 });
 
-  // newUser.on("save", () => {
-  //   console.log(`New user saved!`);
-  // });
+// newUser.on("save", () => {
+//   console.log(`New user saved!`);
+// });
 
-  // await newUser.save();
-}
-
-init();
+// newUser.save();
