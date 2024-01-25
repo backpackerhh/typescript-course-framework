6a0795ca69101374d8dde5ec971fc12cca08689b
David Montesdeoca <backpackerhh@gmail.com>
2024-01-25 11:23:14 +0100

    Add logic to bind events to elements in the DOM

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,3 +1,5 @@
+type EventsMapper = { [key: string]: () => void };
+
 export class UserForm {
   constructor(public parent: HTMLElement) {}
 
@@ -6,6 +8,7 @@ export class UserForm {
       <div>
         <h1>User Form</h1>
         <input type="text" />
+        <button>Save</Button>
       </div>
     `;
   }
@@ -14,6 +17,35 @@ export class UserForm {
     const templateElement = document.createElement("template");
     templateElement.innerHTML = this.template();
 
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
+  }
+
+  eventsMap(): EventsMapper {
+    return {
+      "click:button": this.onClickButton,
+      "mouseenter:h1": this.onMouseEnterHeader,
+    };
+  }
+
+  onClickButton(): void {
+    console.log("Click on button registered!");
+  }
+
+  onMouseEnterHeader(): void {
+    console.log("Hover on h1 registered!");
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (const eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(":");
+      const elements = fragment.querySelectorAll(selector);
+
+      for (const element of elements) {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      }
+    }
   }
 }
