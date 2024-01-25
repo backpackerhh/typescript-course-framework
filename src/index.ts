b0eb738cd081d97251479585d8d803a4e1326027
David Montesdeoca <backpackerhh@gmail.com>
2024-01-25 11:00:04 +0100

    Add basic logic to render form in the browser

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,5 @@
-import { User } from "./models/User";
+import { UserForm } from "./views/UserForm";
 
-const user = User.build({ id: "4ff8" });
-
-user.on("change", () => {
-  console.log(`User with ID ${user.get("id")} fetched! -> ${user.get("name")}`);
-});
-
-user.fetch();
-
-const existingUser = User.build({ id: "4f25", name: "David", age: 37 });
-
-existingUser.on("save", () => {
-  console.log(`User with ID ${existingUser.get("id")} saved! -> ${existingUser.get("name")}`);
-});
-
-existingUser.save();
-
-// const newUser = User.build({ name: "Perico", age: 21 });
-
-// newUser.on("save", () => {
-//   console.log(`New user saved!`);
-// });
-
-// newUser.save();
-
-const usersCollection = User.buildCollection("http://localhost:3000/users");
-
-usersCollection.on("change", () => {
-  console.log(usersCollection);
-});
-
-usersCollection.fetch();
+const rootElement = document.getElementById("root") as HTMLElement;
+const userForm = new UserForm(rootElement);
+userForm.render();
